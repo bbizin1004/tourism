8cc808e2aeba4f087bb38184f4c1c55ef959576a
michelle9876 <kimbg9876@gmail.com>
2025-01-13 19:07:55 +0900

    feature/Add FavPlace & Calendar & CalendarDetails Functions by BoGyeong Kim

--- src/main/java/github/tourism/data/entity/map/Map.java
+++ src/main/java/github/tourism/data/entity/map/Map.java
@@ -1,4 +1,33 @@
 package github.tourism.data.entity.map;
 
+import github.tourism.data.entity.user.User;
+import jakarta.persistence.*;
+import lombok.*;
+
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "Map")
 public class Map {
+    @Id @GeneratedValue
+    @Column(name = "map_id")
+    private int id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id")
+    private User user;
+
+    private double latitude;
+    private double longitude;
+    private String place_name;
+    private String place_image;
+    private String place_info;
+    private String place_details_info;
+    private String operation_time;
+    private String operation_date;
+    private String place_location;
+    private String place_contact_num;
 }
