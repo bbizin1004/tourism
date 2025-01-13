79fdfd7d8c6dc50e5e191dd3b000a98ef8ef81b0
bbizin1004 <bbizin1004@korea.com>
2025-01-14 04:54:32 +0900

    1/14 map 엔티티 수정, map 리포지토리 및 서비스 수정중

--- src/main/java/github/tourism/service/map/MapService.java
+++ src/main/java/github/tourism/service/map/MapService.java
@@ -1,4 +1,22 @@
 package github.tourism.service.map;
 
+
+import github.tourism.data.entity.map.Map;
+import github.tourism.data.repository.map.MapRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
 public class MapService {
+
+    private final MapRepository mapRepository;
+
+    public Page<Map> getAllMaps(Pageable pageable) {
+        return mapRepository.findAll(pageable);
+    }
 }
--- src/main/java/github/tourism/web/controller/map/MapController.java
+++ src/main/java/github/tourism/web/controller/map/MapController.java
@@ -1,4 +1,25 @@
 package github.tourism.web.controller.map;
 
+import github.tourism.data.entity.map.Map;
+import github.tourism.service.map.MapService;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/maps")
 public class MapController {
+
+    private final MapService mapService;
+
+    //전체 관광지 조회
+//    @GetMapping
+//    public Map getMapById(@PathVariable Long id) {
+//        return mapService.getMapById(id);
+//    }
+
+
+
 }
