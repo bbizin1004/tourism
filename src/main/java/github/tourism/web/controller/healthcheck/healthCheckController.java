6dbbf66fc654325c9004d25ab6195d5c0e0d331e
bbizin1004 <bbizin1004@korea.com>
2025-02-14 15:33:23 +0900

    2/14 자동배포 설정2

--- src/main/java/github/tourism/web/controller/healthcheck/healthCheckController.java
+++ src/main/java/github/tourism/web/controller/healthcheck/healthCheckController.java
@@ -2,7 +2,9 @@ package github.tourism.web.controller.healthcheck;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class healthCheckController {
 
     @GetMapping("/health")
